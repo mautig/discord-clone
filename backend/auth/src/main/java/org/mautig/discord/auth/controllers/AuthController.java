8c923db4c8186cc9e314f076d20ac9341237d4c7
maudp <mauduong.dev@gmail.com>
2024-07-15 00:26:02 +0700

    feat: init auth service

---
+++ backend/auth/src/main/java/org/mautig/discord/auth/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package org.mautig.discord.auth.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("login")
+    public void login() {
+
+    }
+}
